359365c82eea6ddfa9adea478161f7b322a5b315
Juan José Rodriguez <zoranbow@gmail.com>
2025-06-07 10:18:59 -0500

    envalid & dotenv config

---
+++ env.ts
@@ -0,0 +1,11 @@
+import { cleanEnv, str, num } from "envalid";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = cleanEnv(process.env, {
+  PORT: num(),
+  DATABASE_URL: str(),
+});
+
+export default env;
