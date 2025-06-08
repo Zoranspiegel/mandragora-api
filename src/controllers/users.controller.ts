c47c51085f9c468240626f41718b6b8eeced39ec
Juan José Rodriguez <zoranbow@gmail.com>
2025-06-08 09:17:16 -0500

    db connection & /api/users GET & POST endpoints

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,15 @@
 import type { AsyncController } from "../../types";
+import prisma from "../db";
+import bcrypt from "bcrypt";
 
 export const getAllUsers: AsyncController = async (_req, res, next) => {
   try {
-    const allUsers: string[] = [];
+    const allUsers = await prisma.user.findMany({
+      omit: {
+        password: true,
+      },
+    });
+
     res.status(200).json(allUsers);
   } catch (error) {
     next(error);
@@ -11,9 +18,25 @@ export const getAllUsers: AsyncController = async (_req, res, next) => {
 
 export const createUser: AsyncController = async (req, res, next) => {
   try {
-    const body = req.body;
+    const { name, email, password } = req.body;
 
-    res.status(201).json({ message: "User successfully created", data: body });
+    const saltOrRounds = 10;
+    const hashPassword = await bcrypt.hash(password, saltOrRounds);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+      },
+      omit: {
+        password: true,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ message: "User successfully created", data: newUser });
   } catch (error) {
     next(error);
   }
